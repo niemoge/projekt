81036e6744efa05de2b53201be1fa1d94ceb0d0e
niemoge <zamielski.patryk@wp.pl>
2023-06-22 21:21:06 +0200

    commit3
    tester

---
+++ tester.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Pracownik pracownik = new Pracownik();
+        pracownik.setImie("Jan");
+        pracownik.setNazwisko("Kowalski");
+        pracownik.setWiek(30);
+
+
+        pracownik.przedstawSie();
+
+
+        Produkt produkt = new Produkt();
+        produkt.setNazwa("Telewizor");
+        produkt.setCena(1999.99);
+        produkt.setDostepnaIlosc(10);
+
+
+        produkt.wyswietlInformacje();
+
+
+        Zamowienie zamowienie = new Zamowienie();
+        zamowienie.setNumer(1);
+        zamowienie.setSprzedawca(pracownik);
+        zamowienie.setProdukt(produkt);
+
+
+        zamowienie.wyswietlSzczegoly();
+    }
+}
